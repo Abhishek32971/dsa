924d1f9882a4956a992379dcc34a1bf9e47ff96c
Abhishek32971 <abhishekrp32971@gmail.com>
2024-04-21 00:40:32 +0530

    testing

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        int[] i={0,1,2,2,3,0,4,2};
-        System.out.println((i[0:5].length));
+        int[] i={0,1,2,2,3,0};
+        System.out.println((i.length/2));
     }
 }
